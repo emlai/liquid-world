5baad757e787940790c88e6a25f155fe824cdc34
Emil Laine <laine.emil@gmail.com>
2023-08-23 22:44:14 +0300

    R* tree

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,47 @@
+extern crate rstar;
 extern crate sdl2;
 
-use aabb_quadtree::QuadTree;
+use rstar::{RTree, RTreeObject, AABB};
 use sdl2::event::Event;
 use sdl2::keyboard::{Keycode, Scancode};
 use sdl2::pixels::Color;
 use sdl2::rect::Rect;
-use std::ptr;
 use std::time::Duration;
 
 struct Pos {
     x: f32,
     y: f32,
 }
+#[derive(Copy, Clone)]
+struct RTreePos {
+    x: f32,
+    y: f32,
+    id: usize,
+}
+impl RTreeObject for RTreePos {
+    type Envelope = AABB<[f32; 2]>;
+
+    fn envelope(&self) -> Self::Envelope {
+        AABB::from_point([self.x, self.y])
+    }
+}
+
 const BALL_DIAMETER: f32 = 10.;
 const VISUAL_DIAMETER: u32 = 5;
 
 struct Collision {
-    a: usize,
-    b: usize,
+    a: RTreePos,
+    b: RTreePos,
 }
 
 pub fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
+    let window_width = 1600;
+    let window_height = 900;
 
     let window = video_subsystem
-        .window("rust-sdl2 demo", 1600, 900)
+        .window("rust-sdl2 demo", window_width, window_height)
         .position_centered()
         .build()
         .unwrap();
@@ -33,78 +49,110 @@ pub fn main() {
     let mut canvas = window.into_canvas().build().unwrap();
     let mut event_pump = sdl_context.event_pump().unwrap();
 
-    let mut positions = Vec::new();
+    let mut cursor_x = (window_width / 2) as f32;
+    let mut cursor_y = (window_height / 2) as f32;
+    let mut positions = RTree::<RTreePos>::new();
     let mut velocities = Vec::new();
+    let mut owners = Vec::new();
     let mut colors = Vec::new();
+    colors.push(Color::RGB(255, 0, 0));
+    colors.push(Color::RGB(0, 255, 0));
+    colors.push(Color::RGB(0, 0, 255));
+    colors.push(Color::RGB(0, 255, 255));
+    colors.push(Color::RGB(255, 0, 255));
+    colors.push(Color::RGB(255, 255, 0));
+    colors.push(Color::RGB(255, 255, 255));
+    colors.push(Color::RGB(128, 128, 128));
+    // let mut collisions = Vec::new();
 
-    let spacing = 15;
-    for x in (0..1000).step_by(spacing) {
-        for y in (0..1000).step_by(spacing) {
-            positions.push(Pos {
-                x: x as f32 + 100.,
-                y: y as f32 + 300.,
-            });
-            velocities.push(Pos { x: 0., y: 0. });
-            colors.push(Color::RGB(255, 0, 0));
+    let spacing = 10;
+    let mut counter = 0;
+    let start_positions = [
+        [0, 0],
+        [1, 0],
+        [2, 0],
+        [3, 0],
+        [0, 1],
+        [1, 1],
+        [2, 1],
+        [3, 1],
+    ];
+    for (player_id, start_pos) in start_positions.iter().enumerate() {
+        for x in (0..(window_width / 8)).step_by(spacing) {
+            for y in (0..(window_height / 4)).step_by(spacing) {
+                positions.insert(RTreePos {
+                    x: (start_pos[0] as f32) * (window_width / 4) as f32 + x as f32,
+                    y: (start_pos[1] as f32) * (window_height / 2) as f32 + y as f32,
+                    id: counter,
+                });
+                counter += 1;
+                velocities.push(Pos { x: 0., y: 0. });
+                owners.push(player_id);
+            }
         }
     }
-    for x in (0..1000).step_by(spacing) {
-        for y in (0..1000).step_by(spacing) {
-            positions.push(Pos {
-                x: x as f32 + 900.,
-                y: y as f32 + 300.,
-            });
-            velocities.push(Pos { x: 0., y: 0. });
-            colors.push(Color::RGB(0, 255, 0));
-        }
-    }
+    dbg!(positions.size());
 
     'main_loop: loop {
         canvas.set_draw_color(Color::RGB(0, 0, 0));
         canvas.clear();
-        let cursor_x = positions[0].x;
-        let cursor_y = positions[0].y;
-        for i in 1..positions.len() {
+        for pos in positions.iter() {
             let cursor_follow_speed = 0.001f32;
-            velocities[i].x += (cursor_x - positions[i].x) * cursor_follow_speed;
-            velocities[i].y += (cursor_y - positions[i].y) * cursor_follow_speed;
+            velocities[pos.id].x += (cursor_x - pos.x) * cursor_follow_speed;
+            velocities[pos.id].y += (cursor_y - pos.y) * cursor_follow_speed;
         }
 
-        for i in 0..positions.len() {
-            positions[i].x += velocities[i].x;
-            positions[i].y += velocities[i].y;
-            velocities[i].x *= 0.9;
-            velocities[i].y *= 0.9;
-        }
+        positions = RTree::bulk_load(
+            positions
+                .iter()
+                .map(|p| {
+                    let mut pos = *p;
+                    pos.x += velocities[pos.id].x;
+                    pos.y += velocities[pos.id].y;
+                    velocities[pos.id].x *= 0.9;
+                    velocities[pos.id].y *= 0.9;
+                    pos
+                })
+                .collect(),
+        );
 
-        let mut collisions = Vec::new();
-        for i in 0..positions.len() {
-            for j in 0..positions.len() {
-                if i == j {
+        let mut counter = 0;
+        for a in positions.iter() {
+            for b in positions.locate_in_envelope(&AABB::from_corners(
+                [a.x - BALL_DIAMETER, a.y - BALL_DIAMETER],
+                [a.x + BALL_DIAMETER, a.y + BALL_DIAMETER],
+            )) {
+                counter += 1;
+                if a.id == b.id {
                     continue;
                 }
-                let dx = positions[j].x - positions[i].x;
-                let dy = positions[j].y - positions[i].y;
+                let dx = b.x - a.x;
+                let dy = b.y - a.y;
                 if (dx * dx + dy * dy) < BALL_DIAMETER * BALL_DIAMETER {
-                    collisions.push(Collision { a: i, b: j });
+                    let evasion_speed = 0.1f32;
+                    let dx = (b.x - a.x) * evasion_speed;
+                    let dy = (b.y - a.y) * evasion_speed;
+                    velocities[a.id].x -= dx;
+                    velocities[a.id].y -= dy;
+                    velocities[b.id].x += dx;
+                    velocities[b.id].y += dy;
                 }
             }
         }
+        // dbg!(counter);
 
-        for collision in &collisions {
-            let evasion_speed = 0.01f32;
-            let dx = (positions[collision.b].x - positions[collision.a].x) * evasion_speed;
-            let dy = (positions[collision.b].y - positions[collision.a].y) * evasion_speed;
-            velocities[collision.a].x -= dx;
-            velocities[collision.a].y -= dy;
-            velocities[collision.b].x += dx;
-            velocities[collision.b].y += dy;
-        }
-        for i in 0..positions.len() {
-            canvas.set_draw_color(colors[i]);
+        canvas.set_draw_color(Color::RGB(255, 0, 255));
+        _ = canvas.fill_rect(Rect::new(
+            cursor_x as i32,
+            cursor_y as i32,
+            (BALL_DIAMETER * 2.) as u32,
+            (BALL_DIAMETER * 2.) as u32,
+        ));
+        for i in positions.iter() {
+            canvas.set_draw_color(colors[owners[i.id]]);
             _ = canvas.fill_rect(Rect::new(
-                positions[i].x as i32,
-                positions[i].y as i32,
+                i.x as i32,
+                i.y as i32,
                 VISUAL_DIAMETER,
                 VISUAL_DIAMETER,
             ));
@@ -112,18 +160,18 @@ pub fn main() {
 
         canvas.present();
         let keyboard = event_pump.keyboard_state();
-        let speed = 0.5f32;
+        let cursor_speed = 5f32;
         if keyboard.is_scancode_pressed(Scancode::Left) {
-            velocities[0].x -= speed;
+            cursor_x -= cursor_speed;
         }
         if keyboard.is_scancode_pressed(Scancode::Right) {
-            velocities[0].x += speed;
+            cursor_x += cursor_speed;
         }
         if keyboard.is_scancode_pressed(Scancode::Up) {
-            velocities[0].y -= speed;
+            cursor_y -= cursor_speed;
         }
         if keyboard.is_scancode_pressed(Scancode::Down) {
-            velocities[0].y += speed;
+            cursor_y += cursor_speed;
         }
 
         for event in event_pump.poll_iter() {
