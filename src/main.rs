810a1a7806095390e40d16ddafba1497ca11bda7
Emil Laine <laine.emil@gmail.com>
2023-08-23 21:31:08 +0300

    SOA

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,19 @@
 extern crate sdl2;
 
+use aabb_quadtree::QuadTree;
 use sdl2::event::Event;
 use sdl2::keyboard::{Keycode, Scancode};
 use sdl2::pixels::Color;
-use sdl2::rect::{Point, Rect};
+use sdl2::rect::Rect;
 use std::ptr;
 use std::time::Duration;
 
-struct Ball {
+struct Pos {
     x: f32,
     y: f32,
-    vx: f32,
-    vy: f32,
-    color: Color,
 }
-const BALL_DIAMETER: f32 = 20.;
-
-// fn distance(a: &Ball, b: &Ball) -> f32 {
-// }
+const BALL_DIAMETER: f32 = 10.;
+const VISUAL_DIAMETER: u32 = 5;
 
 struct Collision {
     a: usize,
@@ -37,115 +33,97 @@ pub fn main() {
     let mut canvas = window.into_canvas().build().unwrap();
     let mut event_pump = sdl_context.event_pump().unwrap();
 
-    let mut balls = Vec::new();
-    let spacing = 30;
-    for x in (0..500).step_by(spacing) {
-        for y in (0..500).step_by(spacing) {
-            balls.push(Ball {
+    let mut positions = Vec::new();
+    let mut velocities = Vec::new();
+    let mut colors = Vec::new();
+
+    let spacing = 15;
+    for x in (0..1000).step_by(spacing) {
+        for y in (0..1000).step_by(spacing) {
+            positions.push(Pos {
                 x: x as f32 + 100.,
-                y: y as f32 + 400.,
-                vx: 0.,
-                vy: 0.,
-                color: Color::RGB(255, 0, 0),
-            })
+                y: y as f32 + 300.,
+            });
+            velocities.push(Pos { x: 0., y: 0. });
+            colors.push(Color::RGB(255, 0, 0));
         }
     }
-    for x in (0..500).step_by(spacing) {
-        for y in (0..500).step_by(spacing) {
-            balls.push(Ball {
+    for x in (0..1000).step_by(spacing) {
+        for y in (0..1000).step_by(spacing) {
+            positions.push(Pos {
                 x: x as f32 + 900.,
-                y: y as f32 + 400.,
-                vx: 0.,
-                vy: 0.,
-                color: Color::RGB(0, 255, 0),
-            })
+                y: y as f32 + 300.,
+            });
+            velocities.push(Pos { x: 0., y: 0. });
+            colors.push(Color::RGB(0, 255, 0));
         }
     }
 
-    'running: loop {
+    'main_loop: loop {
         canvas.set_draw_color(Color::RGB(0, 0, 0));
         canvas.clear();
-        let cursor_x = balls[0].x;
-        let cursor_y = balls[0].y;
-        for ball in balls.iter_mut().skip(1) {
+        let cursor_x = positions[0].x;
+        let cursor_y = positions[0].y;
+        for i in 1..positions.len() {
             let cursor_follow_speed = 0.001f32;
-            ball.vx += (cursor_x - ball.x) * cursor_follow_speed;
-            ball.vy += (cursor_y - ball.y) * cursor_follow_speed;
+            velocities[i].x += (cursor_x - positions[i].x) * cursor_follow_speed;
+            velocities[i].y += (cursor_y - positions[i].y) * cursor_follow_speed;
         }
 
-        for ball in &mut balls {
-            ball.x += ball.vx;
-            ball.y += ball.vy;
-            ball.vx *= 0.9;
-            ball.vy *= 0.9;
+        for i in 0..positions.len() {
+            positions[i].x += velocities[i].x;
+            positions[i].y += velocities[i].y;
+            velocities[i].x *= 0.9;
+            velocities[i].y *= 0.9;
         }
 
         let mut collisions = Vec::new();
-        for (ball_index, ball) in balls.iter().enumerate() {
-            for (other_ball_index, other_ball) in balls.iter().enumerate() {
-                if ptr::eq(&other_ball, &ball) {
+        for i in 0..positions.len() {
+            for j in 0..positions.len() {
+                if i == j {
                     continue;
                 }
-                let dx = other_ball.x - ball.x;
-                let dy = other_ball.y - ball.y;
+                let dx = positions[j].x - positions[i].x;
+                let dy = positions[j].y - positions[i].y;
                 if (dx * dx + dy * dy) < BALL_DIAMETER * BALL_DIAMETER {
-                    collisions.push(Collision {
-                        a: ball_index,
-                        b: other_ball_index,
-                    });
+                    collisions.push(Collision { a: i, b: j });
                 }
             }
         }
 
         for collision in &collisions {
-            let a = &balls[collision.a];
-            let b = &balls[collision.b];
             let evasion_speed = 0.01f32;
-            let dx = (b.x - a.x) * evasion_speed;
-            let dy = (b.y - a.y) * evasion_speed;
-            {
-                let a = &mut balls[collision.a];
-                a.vx -= dx;
-                a.vy -= dy;
-            }
-            {
-                let b = &mut balls[collision.b];
-                b.vx += dx;
-                b.vy += dy;
-            }
+            let dx = (positions[collision.b].x - positions[collision.a].x) * evasion_speed;
+            let dy = (positions[collision.b].y - positions[collision.a].y) * evasion_speed;
+            velocities[collision.a].x -= dx;
+            velocities[collision.a].y -= dy;
+            velocities[collision.b].x += dx;
+            velocities[collision.b].y += dy;
         }
-        for ball in &balls {
-            canvas.set_draw_color(ball.color);
-            _ = canvas.fill_rect(Rect::new(ball.x as i32, ball.y as i32, 10, 10));
+        for i in 0..positions.len() {
+            canvas.set_draw_color(colors[i]);
+            _ = canvas.fill_rect(Rect::new(
+                positions[i].x as i32,
+                positions[i].y as i32,
+                VISUAL_DIAMETER,
+                VISUAL_DIAMETER,
+            ));
         }
-        // _ = canvas.draw_points(
-        //     &*balls
-        //         .iter()
-        //         .map(|ball| {
-        //             Point::new(
-        //                 ball.x as i32,
-        //                 ball.y as i32,
-        //                 // BALL_DIAMETER as u32,
-        //                 // BALL_DIAMETER as u32,
-        //             )
-        //         })
-        //         .collect::<Vec<_>>(),
-        // );
 
         canvas.present();
         let keyboard = event_pump.keyboard_state();
         let speed = 0.5f32;
         if keyboard.is_scancode_pressed(Scancode::Left) {
-            balls[0].vx -= speed;
+            velocities[0].x -= speed;
         }
         if keyboard.is_scancode_pressed(Scancode::Right) {
-            balls[0].vx += speed;
+            velocities[0].x += speed;
         }
         if keyboard.is_scancode_pressed(Scancode::Up) {
-            balls[0].vy -= speed;
+            velocities[0].y -= speed;
         }
         if keyboard.is_scancode_pressed(Scancode::Down) {
-            balls[0].vy += speed;
+            velocities[0].y += speed;
         }
 
         for event in event_pump.poll_iter() {
@@ -154,7 +132,7 @@ pub fn main() {
                 | Event::KeyDown {
                     keycode: Some(Keycode::Escape),
                     ..
-                } => break 'running,
+                } => break 'main_loop,
                 _ => {}
             }
         }
