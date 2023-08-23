6c9039ec4e76e4548ea90ff432309873b278ec16
Emil Laine <laine.emil@gmail.com>
2023-08-23 22:51:53 +0300

    keep same color together

--- src/main.rs
+++ src/main.rs
@@ -26,8 +26,8 @@ impl RTreeObject for RTreePos {
     }
 }
 
-const BALL_DIAMETER: f32 = 10.;
-const VISUAL_DIAMETER: u32 = 5;
+const BALL_DIAMETER: f32 = 8.;
+const VISUAL_DIAMETER: u32 = 6;
 
 struct Collision {
     a: RTreePos,
@@ -49,142 +49,152 @@ pub fn main() {
     let mut canvas = window.into_canvas().build().unwrap();
     let mut event_pump = sdl_context.event_pump().unwrap();
 
-    let mut cursor_x = (window_width / 2) as f32;
-    let mut cursor_y = (window_height / 2) as f32;
-    let mut positions = RTree::<RTreePos>::new();
-    let mut velocities = Vec::new();
-    let mut owners = Vec::new();
-    let mut colors = Vec::new();
-    colors.push(Color::RGB(255, 0, 0));
-    colors.push(Color::RGB(0, 255, 0));
-    colors.push(Color::RGB(0, 0, 255));
-    colors.push(Color::RGB(0, 255, 255));
-    colors.push(Color::RGB(255, 0, 255));
-    colors.push(Color::RGB(255, 255, 0));
-    colors.push(Color::RGB(255, 255, 255));
-    colors.push(Color::RGB(128, 128, 128));
-    // let mut collisions = Vec::new();
+    'restart: loop {
+        let mut cursor_x = (window_width / 2) as f32;
+        let mut cursor_y = (window_height / 2) as f32;
+        let mut positions = RTree::<RTreePos>::new();
+        let mut velocities = Vec::new();
+        let mut owners = Vec::new();
+        let mut colors = Vec::new();
+        colors.push(Color::RGB(255, 0, 0));
+        colors.push(Color::RGB(0, 255, 0));
+        colors.push(Color::RGB(0, 0, 255));
+        colors.push(Color::RGB(0, 255, 255));
+        colors.push(Color::RGB(255, 0, 255));
+        colors.push(Color::RGB(255, 255, 0));
+        colors.push(Color::RGB(255, 255, 255));
+        colors.push(Color::RGB(128, 128, 128));
+        // let mut collisions = Vec::new();
 
-    let spacing = 10;
-    let mut counter = 0;
-    let start_positions = [
-        [0, 0],
-        [1, 0],
-        [2, 0],
-        [3, 0],
-        [0, 1],
-        [1, 1],
-        [2, 1],
-        [3, 1],
-    ];
-    for (player_id, start_pos) in start_positions.iter().enumerate() {
-        for x in (0..(window_width / 8)).step_by(spacing) {
-            for y in (0..(window_height / 4)).step_by(spacing) {
-                positions.insert(RTreePos {
-                    x: (start_pos[0] as f32) * (window_width / 4) as f32 + x as f32,
-                    y: (start_pos[1] as f32) * (window_height / 2) as f32 + y as f32,
-                    id: counter,
-                });
-                counter += 1;
-                velocities.push(Pos { x: 0., y: 0. });
-                owners.push(player_id);
-            }
-        }
-    }
-    dbg!(positions.size());
-
-    'main_loop: loop {
-        canvas.set_draw_color(Color::RGB(0, 0, 0));
-        canvas.clear();
-        for pos in positions.iter() {
-            let cursor_follow_speed = 0.001f32;
-            velocities[pos.id].x += (cursor_x - pos.x) * cursor_follow_speed;
-            velocities[pos.id].y += (cursor_y - pos.y) * cursor_follow_speed;
-        }
-
-        positions = RTree::bulk_load(
-            positions
-                .iter()
-                .map(|p| {
-                    let mut pos = *p;
-                    pos.x += velocities[pos.id].x;
-                    pos.y += velocities[pos.id].y;
-                    velocities[pos.id].x *= 0.9;
-                    velocities[pos.id].y *= 0.9;
-                    pos
-                })
-                .collect(),
-        );
-
+        let spacing = 10;
         let mut counter = 0;
-        for a in positions.iter() {
-            for b in positions.locate_in_envelope(&AABB::from_corners(
-                [a.x - BALL_DIAMETER, a.y - BALL_DIAMETER],
-                [a.x + BALL_DIAMETER, a.y + BALL_DIAMETER],
-            )) {
-                counter += 1;
-                if a.id == b.id {
-                    continue;
-                }
-                let dx = b.x - a.x;
-                let dy = b.y - a.y;
-                if (dx * dx + dy * dy) < BALL_DIAMETER * BALL_DIAMETER {
-                    let evasion_speed = 0.1f32;
-                    let dx = (b.x - a.x) * evasion_speed;
-                    let dy = (b.y - a.y) * evasion_speed;
-                    velocities[a.id].x -= dx;
-                    velocities[a.id].y -= dy;
-                    velocities[b.id].x += dx;
-                    velocities[b.id].y += dy;
+        let start_positions = [
+            [0, 0],
+            [1, 0],
+            [2, 0],
+            [3, 0],
+            [0, 1],
+            [1, 1],
+            [2, 1],
+            [3, 1],
+        ];
+        for (player_id, start_pos) in start_positions.iter().enumerate() {
+            for x in (0..(window_width / 8)).step_by(spacing) {
+                for y in (0..(window_height / 4)).step_by(spacing) {
+                    positions.insert(RTreePos {
+                        x: (start_pos[0] as f32) * (window_width / 4) as f32 + x as f32,
+                        y: (start_pos[1] as f32) * (window_height / 2) as f32 + y as f32,
+                        id: counter,
+                    });
+                    counter += 1;
+                    velocities.push(Pos { x: 0., y: 0. });
+                    owners.push(player_id);
                 }
             }
         }
-        // dbg!(counter);
+        dbg!(positions.size());
 
-        canvas.set_draw_color(Color::RGB(255, 0, 255));
-        _ = canvas.fill_rect(Rect::new(
-            cursor_x as i32,
-            cursor_y as i32,
-            (BALL_DIAMETER * 2.) as u32,
-            (BALL_DIAMETER * 2.) as u32,
-        ));
-        for i in positions.iter() {
-            canvas.set_draw_color(colors[owners[i.id]]);
+        'main_loop: loop {
+            canvas.set_draw_color(Color::RGB(0, 0, 0));
+            canvas.clear();
+            for pos in positions.iter() {
+                let cursor_follow_speed = 0.001f32;
+                velocities[pos.id].x += (cursor_x - pos.x) * cursor_follow_speed;
+                velocities[pos.id].y += (cursor_y - pos.y) * cursor_follow_speed;
+            }
+
+            positions = RTree::bulk_load(
+                positions
+                    .iter()
+                    .map(|p| {
+                        let mut pos = *p;
+                        pos.x += velocities[pos.id].x;
+                        pos.y += velocities[pos.id].y;
+                        velocities[pos.id].x *= 0.9;
+                        velocities[pos.id].y *= 0.9;
+                        pos
+                    })
+                    .collect(),
+            );
+
+            let mut counter = 0;
+            for a in positions.iter() {
+                for b in positions.locate_in_envelope(&AABB::from_corners(
+                    [a.x - BALL_DIAMETER, a.y - BALL_DIAMETER],
+                    [a.x + BALL_DIAMETER, a.y + BALL_DIAMETER],
+                )) {
+                    counter += 1;
+                    if a.id == b.id {
+                        continue;
+                    }
+                    let dx = b.x - a.x;
+                    let dy = b.y - a.y;
+                    if (dx * dx + dy * dy) < BALL_DIAMETER * BALL_DIAMETER {
+                        let evasion_speed = if owners[a.id] != owners[b.id] {
+                            0.1f32
+                        } else {
+                            0.01f32
+                        };
+                        let dx = (b.x - a.x) * evasion_speed;
+                        let dy = (b.y - a.y) * evasion_speed;
+                        velocities[a.id].x -= dx;
+                        velocities[a.id].y -= dy;
+                        velocities[b.id].x += dx;
+                        velocities[b.id].y += dy;
+                    }
+                }
+            }
+            // dbg!(counter);
+
+            canvas.set_draw_color(Color::RGB(255, 0, 255));
             _ = canvas.fill_rect(Rect::new(
-                i.x as i32,
-                i.y as i32,
-                VISUAL_DIAMETER,
-                VISUAL_DIAMETER,
+                cursor_x as i32,
+                cursor_y as i32,
+                (BALL_DIAMETER * 2.) as u32,
+                (BALL_DIAMETER * 2.) as u32,
             ));
-        }
-
-        canvas.present();
-        let keyboard = event_pump.keyboard_state();
-        let cursor_speed = 5f32;
-        if keyboard.is_scancode_pressed(Scancode::Left) {
-            cursor_x -= cursor_speed;
-        }
-        if keyboard.is_scancode_pressed(Scancode::Right) {
-            cursor_x += cursor_speed;
-        }
-        if keyboard.is_scancode_pressed(Scancode::Up) {
-            cursor_y -= cursor_speed;
-        }
-        if keyboard.is_scancode_pressed(Scancode::Down) {
-            cursor_y += cursor_speed;
-        }
-
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => break 'main_loop,
-                _ => {}
+            for i in positions.iter() {
+                canvas.set_draw_color(colors[owners[i.id]]);
+                _ = canvas.fill_rect(Rect::new(
+                    i.x as i32,
+                    i.y as i32,
+                    VISUAL_DIAMETER,
+                    VISUAL_DIAMETER,
+                ));
             }
-        }
 
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 120));
+            canvas.present();
+            let keyboard = event_pump.keyboard_state();
+            let cursor_speed = 5f32;
+            if keyboard.is_scancode_pressed(Scancode::Left) {
+                cursor_x -= cursor_speed;
+            }
+            if keyboard.is_scancode_pressed(Scancode::Right) {
+                cursor_x += cursor_speed;
+            }
+            if keyboard.is_scancode_pressed(Scancode::Up) {
+                cursor_y -= cursor_speed;
+            }
+            if keyboard.is_scancode_pressed(Scancode::Down) {
+                cursor_y += cursor_speed;
+            }
+
+            for event in event_pump.poll_iter() {
+                match event {
+                    Event::Quit { .. }
+                    | Event::KeyDown {
+                        keycode: Some(Keycode::Escape),
+                        ..
+                    } => break 'restart,
+                    Event::KeyDown {
+                        keycode: Some(Keycode::Space),
+                        ..
+                    } => break 'main_loop,
+                    _ => {}
+                }
+            }
+
+            ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 120));
+        }
     }
 }
