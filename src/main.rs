e9c220ba5588ac1dd738378405028d0dbe144c83
Emil Laine <laine.emil@gmail.com>
2023-08-25 20:46:04 +0300

    remove id from Pos

--- src/main.rs
+++ src/main.rs
@@ -16,12 +16,7 @@ struct Keys {
     up: Scancode,
     down: Scancode,
 }
-#[derive(Copy, Clone)]
-struct Pos {
-    pos: Vec2,
-    id: usize, // needed anymore?
-}
-
+type Id = u16;
 const VISUAL_DIAMETER: u32 = 6;
 const BALL_DIAMETER: f32 = 8.;
 const BUCKET_SIZE: i32 = BALL_DIAMETER as i32;
@@ -35,7 +30,6 @@ fn get_bucket_index(bucket_pos: IVec2) -> usize {
 }
 
 pub fn main() {
-    dbg!(BUCKET_SIZE, BUCKETS_PER_COLUMN, BUCKETS_PER_ROW);
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
@@ -113,12 +107,12 @@ pub fn main() {
         let mut velocities = Vec::new();
         let mut owners = Vec::new();
         let mut buckets = Vec::new();
-        for _ in 0..(BUCKETS_PER_ROW * BUCKETS_PER_COLUMN) {
-            buckets.push(SmallVec::<[usize; 16]>::new()); // Make this smallvec?
-        }
+        buckets.resize(
+            (BUCKETS_PER_ROW * BUCKETS_PER_COLUMN) as usize,
+            SmallVec::<[Id; 12]>::new(),
+        );
 
         let spacing = 10;
-        let mut counter = 0;
         let start_positions = [
             [0, 0],
             [1, 0],
@@ -129,24 +123,22 @@ pub fn main() {
             [2, 1],
             [3, 1],
         ];
+        let mut id_counter = 0;
         for (player_id, start_pos) in start_positions.iter().enumerate() {
             for x in (0..(LEVEL_WIDTH / 5)).step_by(spacing) {
                 for y in (0..(LEVEL_HEIGHT / 3)).step_by(spacing) {
-                    let pos = Pos {
-                        pos: Vec2::new(
-                            (start_pos[0] as f32) * (LEVEL_WIDTH / 4) as f32 + x as f32,
-                            (start_pos[1] as f32) * (LEVEL_HEIGHT / 2) as f32 + y as f32,
-                        ),
-                        id: counter,
-                    };
+                    let pos = Vec2::new(
+                        (start_pos[0] as f32) * (LEVEL_WIDTH / 4) as f32 + x as f32,
+                        (start_pos[1] as f32) * (LEVEL_HEIGHT / 2) as f32 + y as f32,
+                    );
                     if cursors.len() == player_id {
-                        cursors.push(Vec2::new(pos.pos.x + 200., pos.pos.y + 200.));
+                        cursors.push(Vec2::new(pos.x + 200., pos.y + 200.));
                     }
                     positions.push(pos);
-                    let bucket_pos = pos.pos.as_ivec2() / BUCKET_SIZE;
-                    buckets[get_bucket_index(bucket_pos)].push(pos.id);
-                    counter += 1;
-                    velocities.push(Vec2 { x: 0., y: 0. });
+                    let bucket_pos = pos.as_ivec2() / BUCKET_SIZE;
+                    buckets[get_bucket_index(bucket_pos)].push(id_counter);
+                    id_counter += 1;
+                    velocities.push(Vec2::ZERO);
                     owners.push(player_id);
                 }
             }
@@ -158,19 +150,17 @@ pub fn main() {
         'main_loop: loop {
             canvas.set_draw_color(Color::RGB(0, 0, 0));
             canvas.clear();
-            for pos in &positions {
+
+            for (i, &pos) in positions.iter().enumerate() {
                 let cursor_follow_speed = 0.001f32;
-                velocities[pos.id].x +=
-                    (cursors[owners[pos.id]].x - pos.pos.x) * cursor_follow_speed;
-                velocities[pos.id].y +=
-                    (cursors[owners[pos.id]].y - pos.pos.y) * cursor_follow_speed;
+                velocities[i].x += (cursors[owners[i]].x - pos.x) * cursor_follow_speed;
+                velocities[i].y += (cursors[owners[i]].y - pos.y) * cursor_follow_speed;
             }
 
             for (i, pos) in positions.iter_mut().enumerate() {
-                // let _actual_bucket = buckets.iter().position(|b| b.contains(&pos.id));
-                let new_pos = pos.pos + velocities[pos.id];
-                velocities[pos.id] *= 0.9;
-                let old_bucket_pos = pos.pos.as_ivec2() / BUCKET_SIZE;
+                let new_pos = *pos + velocities[i];
+                velocities[i] *= 0.9;
+                let old_bucket_pos = pos.as_ivec2() / BUCKET_SIZE;
                 let new_bucket_pos = new_pos.as_ivec2() / BUCKET_SIZE;
                 if new_bucket_pos.x < 0
                     || new_bucket_pos.x >= BUCKETS_PER_ROW
@@ -180,50 +170,45 @@ pub fn main() {
                     continue;
                 }
                 if old_bucket_pos != new_bucket_pos {
-                    // faster with or without this `if`?
                     let old_bucket = &mut buckets[get_bucket_index(old_bucket_pos)];
-                    let index = old_bucket.iter().position(|id| *id == pos.id).unwrap();
+                    let index = old_bucket.iter().position(|id| *id == i as Id).unwrap();
                     old_bucket.remove(index);
                     let new_bucket = &mut buckets[get_bucket_index(new_bucket_pos)];
-                    new_bucket.push(pos.id);
+                    new_bucket.push(i as Id);
                 }
-                pos.pos = new_pos;
-                // update_pos(positions, i, );
+                *pos = new_pos;
             }
 
-            for a in &positions {
-                let bucket_pos = a.pos.as_ivec2() / BUCKET_SIZE;
-                for bucket_x in (bucket_pos.x - 1)..=(bucket_pos.x + 1) {
-                    for bucket_y in (bucket_pos.y - 1)..=(bucket_pos.y + 1) {
-                        if bucket_x < 0
-                            || bucket_x >= BUCKETS_PER_ROW
-                            || bucket_y < 0
-                            || bucket_y >= BUCKETS_PER_COLUMN
-                        {
-                            continue;
-                        }
+            for (a_id, a) in positions.iter().enumerate() {
+                let bucket_pos = a.as_ivec2() / BUCKET_SIZE;
+                for bucket_x in
+                    (bucket_pos.x - 1).max(0)..=(bucket_pos.x + 1).min(BUCKETS_PER_ROW - 1)
+                {
+                    for bucket_y in
+                        (bucket_pos.y - 1).max(0)..=(bucket_pos.y + 1).min(BUCKETS_PER_COLUMN - 1)
+                    {
                         for &b_id in &buckets[get_bucket_index(IVec2 {
                             x: bucket_x,
                             y: bucket_y,
                         })] {
-                            if a.id == b_id {
+                            if a_id == b_id as usize {
                                 continue;
                             }
-                            let b_pos = positions[b_id].pos;
-                            let dx = b_pos.x - a.pos.x;
-                            let dy = b_pos.y - a.pos.y;
+                            let b = positions[b_id as usize];
+                            let dx = b.x - a.x;
+                            let dy = b.y - a.y;
                             if (dx * dx + dy * dy) < BALL_DIAMETER * BALL_DIAMETER {
-                                let evasion_speed = if owners[a.id] != owners[b_id] {
+                                let evasion_speed = if owners[a_id] != owners[b_id as usize] {
                                     0.1f32
                                 } else {
                                     0.05f32
                                 };
-                                let dx = (b_pos.x - a.pos.x) * evasion_speed;
-                                let dy = (b_pos.y - a.pos.y) * evasion_speed;
-                                velocities[a.id].x -= dx;
-                                velocities[a.id].y -= dy;
-                                velocities[b_id].x += dx;
-                                velocities[b_id].y += dy;
+                                let dx = (b.x - a.x) * evasion_speed;
+                                let dy = (b.y - a.y) * evasion_speed;
+                                velocities[a_id].x -= dx;
+                                velocities[a_id].y -= dy;
+                                velocities[b_id as usize].x += dx;
+                                velocities[b_id as usize].y += dy;
                             }
                         }
                     }
@@ -239,11 +224,11 @@ pub fn main() {
                     (BALL_DIAMETER * 2.) as u32,
                 ));
             }
-            for pos in positions.iter() {
-                canvas.set_draw_color(colors[owners[pos.id]]);
+            for (i, pos) in positions.iter().enumerate() {
+                canvas.set_draw_color(colors[owners[i]]);
                 _ = canvas.fill_rect(Rect::new(
-                    pos.pos.x as i32,
-                    pos.pos.y as i32,
+                    pos.x as i32,
+                    pos.y as i32,
                     VISUAL_DIAMETER,
                     VISUAL_DIAMETER,
                 ));
@@ -257,11 +242,6 @@ pub fn main() {
             canvas.set_draw_color(Color::RGB(0, 0, 255));
             _ = canvas.draw_rect(Rect::new(0, 0, 200, 10));
             _ = canvas.draw_rect(Rect::new(0, 0, 400, 10));
-
-            let spilled_count = buckets.iter().filter(|b| b.spilled()).count();
-            if spilled_count > 0 {
-                println!("{}", spilled_count);
-            }
 
             canvas.present();
             let keyboard = event_pump.keyboard_state();
