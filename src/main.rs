843198a0bb222a05eb0ff6c888cbdc48b99eb189
Emil Laine <laine.emil@gmail.com>
2023-08-25 21:40:05 +0300

    adjust constants

--- src/main.rs
+++ src/main.rs
@@ -17,17 +17,21 @@ struct Keys {
     down: Scancode,
 }
 type Id = u16;
-const VISUAL_DIAMETER: u32 = 6;
+const VISUAL_DIAMETER: u32 = 4;
 const BALL_DIAMETER: f32 = 8.;
 const BUCKET_SIZE: i32 = BALL_DIAMETER as i32;
 const BUCKETS_PER_ROW: i32 = LEVEL_WIDTH / BUCKET_SIZE;
 const BUCKETS_PER_COLUMN: i32 = LEVEL_HEIGHT / BUCKET_SIZE;
-const LEVEL_WIDTH: i32 = 1600;
-const LEVEL_HEIGHT: i32 = 896; // to fit even number of buckets
 
-fn get_bucket_index(bucket_pos: IVec2) -> usize {
-    (bucket_pos.x + bucket_pos.y * BUCKETS_PER_ROW) as usize
-}
+// these should fit an even number of buckets, just in case
+const LEVEL_WIDTH: i32 = 1728;
+const LEVEL_HEIGHT: i32 = 1048;
+
+const ENEMY_EVASION: f32 = 0.05f32;
+const FRIENDLY_EVASION: f32 = 0.01f32;
+const VELOCITY_SLOWDOWN: f32 = 0.9;
+const CURSOR_FOLLOW_SPEED: f32 = 0.002f32;
+const CURSOR_SPEED: f32 = 2f32;
 
 pub fn main() {
     let sdl_context = sdl2::init().unwrap();
@@ -112,7 +116,7 @@ pub fn main() {
             SmallVec::<[Id; 12]>::new(),
         );
 
-        let spacing = 10;
+        let spacing = BALL_DIAMETER as usize;
         let start_positions = [
             [0, 0],
             [1, 0],
@@ -125,14 +129,14 @@ pub fn main() {
         ];
         let mut id_counter = 0;
         for (player_id, start_pos) in start_positions.iter().enumerate() {
-            for x in (0..(LEVEL_WIDTH / 5)).step_by(spacing) {
-                for y in (0..(LEVEL_HEIGHT / 3)).step_by(spacing) {
+            for x in (0..(LEVEL_WIDTH / 6)).step_by(spacing) {
+                for y in (0..(LEVEL_HEIGHT / 4)).step_by(spacing) {
                     let pos = Vec2::new(
                         (start_pos[0] as f32) * (LEVEL_WIDTH / 4) as f32 + x as f32,
                         (start_pos[1] as f32) * (LEVEL_HEIGHT / 2) as f32 + y as f32,
                     );
                     if cursors.len() == player_id {
-                        cursors.push(Vec2::new(pos.x + 200., pos.y + 200.));
+                        cursors.push(Vec2::new(pos.x + 150., pos.y + 150.));
                     }
                     positions.push(pos);
                     let bucket_pos = pos.as_ivec2() / BUCKET_SIZE;
@@ -152,14 +156,13 @@ pub fn main() {
             canvas.clear();
 
             for (i, &pos) in positions.iter().enumerate() {
-                let cursor_follow_speed = 0.001f32;
-                velocities[i].x += (cursors[owners[i]].x - pos.x) * cursor_follow_speed;
-                velocities[i].y += (cursors[owners[i]].y - pos.y) * cursor_follow_speed;
+                velocities[i].x += (cursors[owners[i]].x - pos.x) * CURSOR_FOLLOW_SPEED;
+                velocities[i].y += (cursors[owners[i]].y - pos.y) * CURSOR_FOLLOW_SPEED;
             }
 
             for (i, pos) in positions.iter_mut().enumerate() {
                 let new_pos = *pos + velocities[i];
-                velocities[i] *= 0.9;
+                velocities[i] *= VELOCITY_SLOWDOWN;
                 let old_bucket_pos = pos.as_ivec2() / BUCKET_SIZE;
                 let new_bucket_pos = new_pos.as_ivec2() / BUCKET_SIZE;
                 if new_bucket_pos.x < 0
@@ -199,9 +202,9 @@ pub fn main() {
                             let dy = b.y - a.y;
                             if (dx * dx + dy * dy) < BALL_DIAMETER * BALL_DIAMETER {
                                 let evasion_speed = if owners[a_id] != owners[b_id as usize] {
-                                    0.1f32
+                                    ENEMY_EVASION
                                 } else {
-                                    0.05f32
+                                    FRIENDLY_EVASION
                                 };
                                 let dx = (b.x - a.x) * evasion_speed;
                                 let dy = (b.y - a.y) * evasion_speed;
@@ -245,19 +248,18 @@ pub fn main() {
 
             canvas.present();
             let keyboard = event_pump.keyboard_state();
-            let cursor_speed = 5f32;
             for (id, keys) in player_keys.iter().enumerate() {
                 if keyboard.is_scancode_pressed(keys.left) {
-                    cursors[id].x -= cursor_speed;
+                    cursors[id].x -= CURSOR_SPEED;
                 }
                 if keyboard.is_scancode_pressed(keys.right) {
-                    cursors[id].x += cursor_speed;
+                    cursors[id].x += CURSOR_SPEED;
                 }
                 if keyboard.is_scancode_pressed(keys.up) {
-                    cursors[id].y -= cursor_speed;
+                    cursors[id].y -= CURSOR_SPEED;
                 }
                 if keyboard.is_scancode_pressed(keys.down) {
-                    cursors[id].y += cursor_speed;
+                    cursors[id].y += CURSOR_SPEED;
                 }
             }
 
@@ -279,4 +281,8 @@ pub fn main() {
             // ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 120));
         }
     }
+}
+
+fn get_bucket_index(bucket_pos: IVec2) -> usize {
+    (bucket_pos.x + bucket_pos.y * BUCKETS_PER_ROW) as usize
 }
